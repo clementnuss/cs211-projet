304640533e3af5b282e72b647fcc82ce827295f2
Leandro Kieliger <leandro.kieliger@epfl.ch>
2016-03-08 16:20:15 +0100

    Optimisation du code de la rotation

--- src/ch/epfl/cs211/objects/Plate.java
+++ src/ch/epfl/cs211/objects/Plate.java
@@ -1,6 +1,6 @@
 package ch.epfl.cs211.objects;
 
-import ch.epfl.cs211.tools.AngleUtils;
+
 import processing.core.PApplet;
 import static java.lang.Math.*;
 import static ch.epfl.cs211.tools.AngleUtils.*;
@@ -10,11 +10,11 @@ import static ch.epfl.cs211.tools.AngleUtils.*;
  */
 public class Plate {
 
-    private int x, y, z;
-    private float angleX, angleY, angleZ;
-    private final PApplet parent;
-
     private final static float MAX_ANGLE = (float)(PI/6.0);
+
+    private int x, y, z;
+    private float angleX, angleY, angleZ, angleStep;
+    private final PApplet parent;
 
     public Plate(int x, int y, int z, PApplet p){
         this.x = x;
@@ -24,6 +24,7 @@ public class Plate {
         this.angleX = 0;
         this.angleY = 0;
         this.angleZ = 0;
+        this.angleStep = 1;
     }
 
     public void display(){
@@ -44,14 +45,14 @@ public class Plate {
         System.out.println("dX: "+deltaX+"dY: "+deltaY);
 
         if(deltaX < 0){
-            angleZ = minClampAngle((float)(angleZ - PI/100.), -MAX_ANGLE);
+            angleZ = minClampAngle(angleZ - angleStep, -MAX_ANGLE);
         }else if(deltaX > 0){
-            angleZ = maxClampAngle((float)(angleZ + PI/100.), MAX_ANGLE);
+            angleZ = maxClampAngle(angleZ + angleStep, MAX_ANGLE);
         }
-        if( deltaY < 0){
-            angleX = maxClampAngle((float)(angleX + PI/100.), MAX_ANGLE);
-        } else if(deltaY > 0){
-            angleX = minClampAngle((float)(angleX - PI/100.), -MAX_ANGLE);
+        if(deltaY < 0){
+            angleX = maxClampAngle(angleX + angleStep, MAX_ANGLE);
+        }else if(deltaY > 0){
+            angleX = minClampAngle(angleX - angleStep, -MAX_ANGLE);
         }
 
     }
