4278452edf8f2a94db88692af4edddb6d09f74f9
Clément Nussbaumer <clement@astutus.org>
2016-03-08 09:30:04 +0100

    étape 3 part II

--- src/ch/epfl/cs211/Assignment3.java
+++ src/ch/epfl/cs211/Assignment3.java
@@ -12,6 +12,7 @@ public class Assignment3 extends PApplet {
         PApplet.main(new String[]{"ch.epfl.cs211.Assignment3"});
     }
 
+    float depth = 1000;
     public void settings() {
         size(500, 500, P3D);
     }
@@ -21,26 +22,35 @@ public class Assignment3 extends PApplet {
     }
 
     public void draw() {
+        camera(width / 2, height / 2, depth, 250, 250, 0, 0, 1, 0);
+        directionalLight(50, 100, 125, 0, -1, 0);
+        ambientLight(102, 102, 102);
         background(200);
-        pushMatrix();
         translate(width / 2, height / 2, 0);
-        rotateX(PI / 8);
-        rotateY(PI / 8);
-        box(100, 80, 60);
-        fill(0xff9a00);
-        translate(100, 0, 0);
-        sphere(50);
-        popMatrix();
-        box(100, 80, 60);
+        float rz = map(mouseY, 0, height, 0, PI);
+        float ry = map(mouseX, 0, width, 0, PI);
+        rotateZ(rz);
+        rotateY(ry);
+        for (int x = -2; x <= 2; x++) {
+            for (int y = -2; y <= 2; y++) {
+                for (int z = -2; z <= 2; z++) {
+                    pushMatrix();
+                    translate(100 * x, 100 * y, -100 * z);
+                    box(50);
+                    popMatrix();
+                }
+            }
+        }
+    }
 
-        camera(mouseX, mouseY, 450, 250, 250, 0, 0, 1, 0);
-        translate(width/2, height/2, 0);
-        rotateX(PI/8);
-        rotateY(PI/8);
-        box(100, 80, 60);
-        translate(100, 0, 0);
-        sphere(50);
-
+    public void keyPressed() {
+        if (key == CODED) {
+            if (keyCode == UP) {
+                depth -= 50;
+            } else if (keyCode == DOWN) {
+                depth += 50;
+            }
+        }
     }
 
 }
