7c84b71b3d53aea57720abf360ae7a10ebf3fd4a
Leandro Kieliger <leandro.kieliger@epfl.ch>
2016-04-07 23:42:41 +0200

    Correction classe Mover

--- src/ch/epfl/cs211/physicsEngine/Mover.java
+++ src/ch/epfl/cs211/physicsEngine/Mover.java
@@ -1,11 +1,10 @@
 /**
- *  Visual Computing project (CS211) - 2016
- *  Authors : Clément Nussbaumer, Leandro Kieliger, Louis Rossier
- *
+ * Visual Computing project (CS211) - 2016
+ * Authors : Clément Nussbaumer, Leandro Kieliger, Louis Rossier
  */
 package ch.epfl.cs211.physicsEngine;
 
-import ch.epfl.cs211.Game;
+
 import ch.epfl.cs211.objects.Plate;
 import ch.epfl.cs211.tools.Color;
 import processing.core.PVector;
@@ -13,51 +12,42 @@ import processing.core.PVector;
 import java.util.List;
 
 import static ch.epfl.cs211.tools.ValueUtils.clamp;
-import static ch.epfl.cs211.Game.GAME;
 import static processing.core.PApplet.sin;
-import static processing.core.PApplet.sqrt;
+import static ch.epfl.cs211.Game.GAME;
 
 public class Mover {
 
-    private final static float GRAVITY_SCALAR = 0.1f;
-    public final static float SPHERE_RADIUS = 20f;
+    private final static float SPHERE_RADIUS = 20f;
     public final static float CYLINDER_RADIUS = 25f;
-    private final static float COLLISION_THRESHOLD = 0.00007f;
-    private final static float MU = 0.01f;
+    public final static float GROUND_OFFSET = -Plate.PLATE_THICKNESS/2 - SPHERE_RADIUS;
+    public final static float SPHERE_TO_CYLINDER_DISTANCE = SPHERE_RADIUS + CYLINDER_RADIUS;
+    private final static float GRAVITY_SCALAR = 9.81f / GAME.frameRate;
+    private final static float FRICTION_FACTOR = 0.02f;
 
-    private PVector pos;
-    private PVector previousPos;
+    private final PVector pos;
 
     private final Plate plate;
     private final float bound;
     private PVector velocity;
-    private PVector gravityForce;
+    private final PVector gravityForce;
 
 
     public Mover(Plate pl) {
         this.plate = pl;
-        this.pos = new PVector(plate.getX(), -(plate.getPlateThickness() / 2f + SPHERE_RADIUS), plate.getZ());
+        this.pos = new PVector(plate.getX(), GROUND_OFFSET, plate.getZ());
         velocity = new PVector(0, 0, 0);
         gravityForce = new PVector(0, 0, 0);
-        bound = plate.getPlateWidth() / 2f - SPHERE_RADIUS;
+        bound = Plate.PLATE_WIDTH / 2f - SPHERE_RADIUS;
     }
 
     public void update() {
-
-        previousPos = pos.copy();
 
         gravityForce.x = sin(plate.getAngleZ()) * GRAVITY_SCALAR;
         gravityForce.z = sin(-plate.getAngleX()) * GRAVITY_SCALAR;
 
         velocity.add(gravityForce);
-
-        float normalForce = 1;
-        float mu = 0.01f;
-        float frictionMagnitude = normalForce * mu;
         PVector friction = velocity.copy();
-        friction.mult(-1);
-        friction.normalize();
-        friction.mult(frictionMagnitude);
+        friction.mult(-FRICTION_FACTOR);
 
         velocity.add(friction);
 
@@ -91,53 +81,54 @@ public class Mover {
 
         if (pos.x > upperBoundX || pos.x < lowerBoundX) {
             pos.x = clamp(pos.x, lowerBoundX, upperBoundX);
-            velocity.x = velocity.x * -1;
+            velocity.x = velocity.x * -0.8f;
             GAME.decScore(velocity.mag());
         }
         if (pos.z > upperBoundZ || pos.z < lowerBoundZ) {
             pos.z = clamp(pos.z, -bound, bound);
-            velocity.z = velocity.z * -1;
+            velocity.z = velocity.z * -0.8f;
             GAME.decScore(velocity.mag());
         }
     }
 
     private void checkCylinders(List<PVector> cylinders) {
-
-        PVector nv = velocity.copy().normalize();
         boolean collisionOccured = false;
 
-        float minimumDistance = CYLINDER_RADIUS + SPHERE_RADIUS;
+        /*
+            Because corrections are additive among cylinders we take copies that will store all
+            correction shifts computed using the original position of the ball.
+        */
+        PVector correctedPos = pos.copy();
+        PVector correctedVel = velocity.copy();
 
         for (PVector cylinderBaseLocation : cylinders) {
 
             PVector cyl = cylinderBaseLocation.copy().add(0, -SPHERE_RADIUS, 0);
             float distance = cyl.dist(pos);
-            if (distance < minimumDistance) {
+
+            if (distance <= SPHERE_TO_CYLINDER_DISTANCE) {
                 collisionOccured = true;
-                PVector ballToCyl = PVector.sub(cyl, previousPos);
-                float projOnVelocity = PVector.dot(nv, ballToCyl);
+                PVector cylinderToBall = PVector.sub(pos, cyl);
 
-                float distanceOfProjSquared = ballToCyl.magSq() - (projOnVelocity * projOnVelocity);
-                float illegalCrossingLength = sqrt((minimumDistance * minimumDistance) - distanceOfProjSquared);
-
-                PVector correctedPos = PVector.mult(nv, projOnVelocity - illegalCrossingLength);
-
-                //performs shifting instead of finding latest legal value for position so it's cumulative among cylinders
-                correctedPos.add(previousPos);
-                PVector shifting = PVector.sub(correctedPos, pos);
-                pos.add(shifting);
+                /*
+                    Ball entered cylinder, push it outwards in a radial fashion.
+                    This is not as accurate as finding the last valid position but it requires a lot
+                    less computation and the loss of realism is practically invisible.
+                 */
+                correctedPos.add(PVector.mult(cylinderToBall.copy().normalize(), (SPHERE_TO_CYLINDER_DISTANCE - cylinderToBall.mag())));
 
                 PVector collisionNormal = new PVector(pos.x - cyl.x, 0, pos.z - cyl.z).normalize();
-                PVector updatedVel = PVector.mult(collisionNormal, 1.9f * velocity.dot(collisionNormal));
-                velocity.sub(updatedVel.x, 0, updatedVel.z);
-
-                GAME.incScore(velocity.mag());
+                PVector updatedVel = PVector.mult(collisionNormal, 2f * velocity.dot(collisionNormal));
+                correctedVel.sub(updatedVel.x, 0, updatedVel.z);
             }
         }
-        //This is to avoid glitches where the ball can't move but velocities keep accumulating
-        if(collisionOccured && PVector.dist(previousPos, pos) < COLLISION_THRESHOLD){
-            velocity = new PVector(0,0,0);
 
+        if (collisionOccured) {
+            float magVel = velocity.mag();
+            pos.x = correctedPos.x;
+            pos.z = correctedPos.z;
+            velocity = correctedVel.normalize().mult(magVel * 0.8f);
+            GAME.incScore(magVel);
         }
     }
 
