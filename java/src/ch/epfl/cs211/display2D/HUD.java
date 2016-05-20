f907bb5e431955d54d1c87fbc5f6700c581c84bc
Leandro Kieliger <leandro.kieliger@epfl.ch>
2016-05-20 09:20:58 +0200

    Accidentally deleted HUD.java, adding it back

---
+++ java/src/ch/epfl/cs211/display2D/HUD.java
@@ -0,0 +1,41 @@
+/**
+ *  Visual Computing project (CS211) - 2016
+ *  Authors : Clément Nussbaumer, Leandro Kieliger, Louis Rossier
+ *
+ */
+package ch.epfl.cs211.display2D;
+
+import ch.epfl.cs211.Game;
+
+public class HUD {
+
+    private int x, y, width, height, z;
+    private int color;
+
+    /**
+     *
+     * @param x HUD box x position
+     * @param y HUD box y position
+     * @param width    HUD box width
+     * @param height    HUD box height
+     */
+    public HUD(int x, int y, int width, int height, int color) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+        this.color = color;
+    }
+
+    /**
+     *  renders the given text on the HUD
+     *
+     * @param text  the text to be drawn in the HUD
+     */
+    public void display(String text) {
+        Game.GAME.textSize(14);
+        Game.GAME.fill(color);
+        Game.GAME.text(text, x, y, width, height);  // Specify a z-axis value
+    }
+
+}
