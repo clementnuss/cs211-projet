374ed0b317d027b856ab71fc6acab99fc233aace
Leandro Kieliger <leandro.kieliger@epfl.ch>
2016-05-24 11:34:49 +0200

    Work in progress for blob detection

--- java/src/ch/epfl/cs211/VideoCapture/BlobDetection.java
+++ java/src/ch/epfl/cs211/VideoCapture/BlobDetection.java
@@ -4,9 +4,10 @@ import processing.core.PImage;
 import processing.core.PVector;
 
 import java.awt.*;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
-import java.util.TreeSet;
+
+import static ch.epfl.cs211.Game.GAME;
 
 public class BlobDetection {
     Polygon quad = new Polygon();
@@ -29,10 +30,67 @@ public class BlobDetection {
     }
 
     PImage findConnectedComponents(PImage input) {
+        int imageLength = input.width * input.height;
         // First pass: label the pixels and store labels' equivalences
-        int[] labels = new int[input.width * input.height];
+        int[] labels = new int[imageLength];
         List<TreeSet<Integer>> labelsEquivalences = new ArrayList<TreeSet<Integer>>();
+        List<Integer> neighborsList = new ArrayList<>(4);
+        List<Integer> labelsList = new ArrayList<>(4);
         int currentLabel = 1;
+        int yp, prevYp;
+        //The first row and first column always have different labels
+        for(int y=0; y<input.height; y++) {
+            yp = y * input.width;
+            prevYp = (y-1) * input.width;
+            for (int x = 0; x < input.width; x++) {
+                neighborsList.clear();
+                labelsList.clear();
+                if(x==0 || y ==0)
+                    labels[yp + x] = currentLabel++;
+                else {
+                    int C = yp + x;
+                    float cH = GAME.hue(input.pixels[C]);
+
+                    int W = yp + x - 1;
+                    labelsList.add(labels[W]);
+                    float hW = GAME.hue(input.pixels[W]);
+
+                    int NW = prevYp + x -1;
+                    labelsList.add(labels[NW]);
+                    float hNW = GAME.hue(input.pixels[NW]);
+
+                    int N = prevYp + x;
+                    labelsList.add(labels[N]);
+                    float hN = GAME.hue(input.pixels[N]);
+
+                    int NE = prevYp + x + 1;
+                    labelsList.add(labels[NE]);
+                    float hNE = (x==input.width) ? -1 : GAME.hue(input.pixels[NE]);
+
+                    if(cH == hW)
+                        neighborsList.add(W);
+                    if(cH == hNW)
+                        neighborsList.add(NW);
+                    if(cH == hN)
+                        neighborsList.add(N);
+                    if(cH == hNE)
+                        neighborsList.add(NE);
+
+                    if(neighborsList.isEmpty()){
+                        labels[C] = currentLabel++;
+                    } else {
+                        Collections.sort(labelsList);
+                        labels[C] = labelsList.get(0);
+                        boolean found = false;
+                        for(TreeSet set : labelsEquivalences){
+
+                        }
+                    }
+
+                }
+            }
+        }
+
         // TODO!
         // Second pass: re-label the pixels by their equivalent class
         // TODO!
